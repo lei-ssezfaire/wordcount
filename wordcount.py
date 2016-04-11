0a8cc02445c7726bf307dcf92176404c296639d9
Rosie <rosie@riveter13-md.swna.wdpr.disney.com>
2016-04-11 13:00:26 -0700

    Now takes arguments at prompt.

--- wordcount.py
+++ wordcount.py
@@ -1,20 +1,25 @@
-# put your code here.
+# # put your code here.
+
+import sys
 
 def word_counter(file_name):
 	""" Counts words in document. """
 
 	word_dict = {}
-	poem_data = open(file_name)
+	try :
+		poem_data = open(file_name)
+	except:
+		print "Not a valid file."
+		sys.exit(1)
 	for line in poem_data:
 		line = line.rstrip()
 		line = line.lower()
-		poem_line = line.split(" ")
+		poem_line = line.split()
 		for item in poem_line:
-			item = item.strip("\"").strip("?").strip(".").strip(",").strip(";").strip(
-				"!").strip(":").strip("-")
+			item = item.strip("\".,!?-:;_\'")
 			word_dict[item] = word_dict.get(item, 0) + 1
 	for word, word_count in word_dict.iteritems():
 		print "{} {}".format(word, word_count)
 	poem_data.close()
 
-word_counter("test.txt")
+word_counter(sys.argv[1])
