eea0453a1fd17a1544fb96058f9b3e0c7290ee67
Rosie <rosie@riveter13-md.swna.wdpr.disney.com>
2016-04-11 12:12:26 -0700

    Switched to .get method.

--- wordcount.py
+++ wordcount.py
@@ -2,18 +2,15 @@
 
 def word_counter(file_name):
 	""" Counts words in document. """
-	
+
 	word_dict = {}
 	poem_data = open(file_name)
 	for line in poem_data:
 		line = line.rstrip()
 		poem_line = line.split(" ")
 		for item in poem_line:
-			if item in word_dict:
-				word_dict[item] = word_dict[item] + 1
-			else:
-				word_dict[item] = 1
-	for word, word_count in word_dict.items():
+			word_dict[item] = word_dict.get(item, 0) + 1
+	for word, word_count in word_dict.iteritems():
 		print "{} {}".format(word, word_count)
 	poem_data.close()
 
